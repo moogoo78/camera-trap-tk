027e7d7a9cc35229a338aa7d677635aa1d975111
MooGoo <moogoo78@gmail.com>
2022-04-28 11:55:26 +0800

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from bson.objectid import ObjectId
+
+def validate_datetime(datetime_text, datetime_format=''):
+    try:
+        if datetime_text != datetime.strptime(datetime_text, datetime_format).strftime(datetime_format):
+            raise ValueError
+        return True
+    except ValueError:
+        return False
