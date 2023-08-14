2a91e93f5510e9e51e0242cba92863c00d01377e
MooGoo <moogoo78@gmail.com>
2023-08-14 11:22:46 +0800

    add sercets in config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import configparser
 import os
+import json
 
 class Config(configparser.ConfigParser):
     ini_file = None
@@ -8,11 +9,25 @@ class Config(configparser.ConfigParser):
         super().__init__()
 
         self.ini_file = ini_file
+        self.secrets = {
+            'aws_access_key_id': '',
+            'aws_secret_access_key': '',
+        }
         # copy sample if init_file not exists
         if not os.path.exists(self.ini_file):
             self.cp_sample()
 
         self.read(self.ini_file, encoding='utf-8')
+
+        # credentials
+        #if is_publish := self.get('Mode', 'publish', fallback=''):
+        #    if is_publish and str(is_publish) not in ['f', '0', 'false']:
+        with open('credentials.json') as f:
+            cred = json.loads(f.read())
+            if key := cred.get('aws-s3-upload'):
+                self.secrets['aws_access_key_id'] = key['access_key_id']
+                self.secrets['aws_secret_access_key'] = key['secret_access_key']
+
 
     def cp_sample(self):
         init_sample_path = '{}.sample'.format(self.ini_file)
