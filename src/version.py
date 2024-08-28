edf277a0a2cb8223e14272762c062b3e17339880
MooGoo <moogoo78@gmail.com>
2024-08-29 07:48:57 +0800

    version

--- src/version.py
+++ src/version.py
@@ -5,4 +5,4 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = '1.1.11(2024.07.22)'
+__version__ = '1.1.12beta(2024.08.28)'
